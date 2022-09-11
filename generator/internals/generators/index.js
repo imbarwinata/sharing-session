25cbddf9aba62d32bd1ac9d13d7d886dada7b1d0
imbarwinataa <imbar.winata@tiket.com>
2022-09-11 10:06:06 +0700

    feat(generator): add example setActionType and example multiple use helper

--- generator/internals/generators/index.js
+++ generator/internals/generators/index.js
@@ -5,11 +5,15 @@
  */
 const { toSlug } = require('../lib/string')
 const componentsGenerator = require('./components')
+const pagesGenerator = require('./pages')
 const hooksGenerator = require('./hooks')
 
 module.exports = (plop) => {
   plop.setHelper('toSlug', (text) => toSlug(text))
+  plop.setHelper('isIndex', (text) => text === 'index' ? '' : text)
   plop.setPartial('componentName', '{{ properCase type }}{{ properCase name }}{{ properCase variant }}')
+  plop.setActionType('log', (_answers, config) => console.log(config.message))
+  plop.setGenerator('page', pagesGenerator)
   plop.setGenerator('component', componentsGenerator)
   plop.setGenerator('hooks', hooksGenerator)
 }
